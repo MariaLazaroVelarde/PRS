2309746cdf510ee7d57c565eb548ca2bbc025573
CC-VictorCuaresma <118938567+CC-VictorCuaresma@users.noreply.github.com>
2025-08-11 06:50:31 +0000

    Bk-Limpieza

--- src/main/java/pe/edu/vallegrande/ms_distribution/infrastructure/repository/OrganizationRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.vallegrande.ms_distribution.infrastructure.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import pe.edu.vallegrande.ms_distribution.domain.models.Organization;
-
-public interface OrganizationRepository extends ReactiveMongoRepository<Organization, String> {
-}
